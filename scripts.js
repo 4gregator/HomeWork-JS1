a3c8e05f58347bc6e0b7a3d823e4a877460706bd
Ivan Krasnobaev <ikrasnobai@yandex.ru>
2018-09-04 21:44:46 +0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// 1) задание
+var Tf, Tc;
+
+Tf = (9 / 5) * Tc + 32;
+
+// 2) задание
+var int = 555.44483,
+	str = "H3110 W0R1D",
+	boo = true,
+	nll = null,
+	und,
+	obj = {
+		name: "Karl",
+		age: 16,
+		alive: false
+	};
+
+// 3) задание 
+var admin, name;
+name = "Василий";
+admin = name;
+console.log(admin);
+
+// 4) задание, ответ: 1000108
