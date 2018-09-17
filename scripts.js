e5f439f4b355ac723717a1a8939a41be09857755
Ivan Krasnobaev <ikrasnobai@yandex.ru>
2018-09-17 15:59:15 +0500

    HW 4

--- scripts.js
+++ scripts.js
@@ -1,44 +1,52 @@
 // 1)
-let n = 0,
-	nP = [];
+let number = parseInt(prompt("Введите число от 0 до 999")),
+	obj = {};
 
-while (n <= 100) {
-	if (n > 1) {
-		if (nP.length) {
-			let counter = 0;
-			for (let i = 0; i < nP.length; i++) {
-				if (n % nP[i]) counter++;
-				else break;
-			}
-			if (counter == nP.length) nP.push(n);
-		} else nP.push(n);
+function numberToObj(number) {
+	if ( isNaN(number) ) {
+		console.log("Введено не число");
+		return obj;
+	} else if (number > 999) {
+		console.log("Введено число выше указанного диапазона");
+		return obj;
+	} else {
+		obj.units = number % 10;
+		obj.dozens = Math.floor(number / 10) % 10;
+		obj.hundreds = Math.floor(number / 100) % 10;
+		return obj;
 	}
-	n++;
 }
-console.log(nP);
+
+console.log(numberToObj(number));
 
 // 2)
-function evenOdd(n) {
-	let string;
-	if (n) {
-		string = (n % 2) ? n + " - нечетное число" : n + " - четное число";
-	} else string = n + " - это ноль";
-	return string;
+let basket = [
+	{
+		id: 5468,
+		productName: "Кирпич, м3",
+		quantity: 15,
+		price: 45
+	}, {
+		id: 7124,
+		productName: "Брус 40х40, м3",
+		quantity: 4,
+		price: 128
+	}, {
+		id: 6781,
+		productName: "Книга 'строительство для чайников', шт",
+		quantity: 1,
+		price: 14.2
+	}
+];
+
+function countBasketPrice(basket) {
+	let sum = 0;
+
+	for (let i = 0; i < basket.length; i++) {
+		sum += basket[i].quantity * basket[i].price;
+	}
+
+	return sum;
 }
 
-n = 0;
-do {
-	console.log(evenOdd(n));
-	n++;
-} while (n <= 10);
-
-// 3)
-for (let i = 0; i < 10; console.log(i++)) {}
-
-// 4)
-let x = "";
-
-for (let i = 1; i <= 20; i++) {
-	x = x + "x";
-	console.log(x);
-}
+console.log(countBasketPrice(basket));
